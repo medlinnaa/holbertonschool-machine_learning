d345d138a6985ff38cb7066bde537f126c2682bf
medlinnaa <madinamamishova@gmail.com>
2026-01-15 19:04:04 +0000

    a function def concat(df1, df2): that takes two pd.DataFrame objects

---
+++ pipeline/pandas/11-concat.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""11-concat.py"""
+
+import pandas as pd
+index = __import__('10-index').index
+
+
+def concat(df1, df2):
+    df1 = index(df1)
+    df2 = index(df2)
+
+    df2 = df2.loc[:1417411920]
+
+    return pd.concat([df2, df1], keys=["bitstamp", "coinbase"])
