eaf3364621ee24f77c918671ba5da4395c80a22e
medlinnaa <madinamamishova@gmail.com>
2026-01-28 11:19:51 +0000

    fourth task done

---
+++ math/linear_algebra/3-flip_me_over.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""defining the function itself """
+
+
+def matrix_transpose(matrix):
+    """ a function that returns the transpose of a matrix using list comprehension """
+    row = len(matrix)
+    col = len(matrix[0])
+    return [[matrix[r][c] for r in range(rows)] for c in range(cols)]
