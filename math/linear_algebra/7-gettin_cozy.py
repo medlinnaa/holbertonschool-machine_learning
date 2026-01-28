e0aedc7641affbf8baf1b467bca00330c24036aa
medlinnaa <madinamamishova@gmail.com>
2026-01-28 18:41:03 +0000

    eighth task done

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -6,12 +6,12 @@ def cat_matrices2D(mat1, mat2, axis=0):
     """a function that returns
     the concatenation of two 2D matrices"""
 
-    if axis === 0:
+    if axis == 0:
         if len(mat1(0)) != len(mat2(0)):
             return None
         return [row[:] for row in mat1] + [row[:] for row in mat2]
 
-    if axis === 1:
+    if axis == 1:
         if len(mat1) != len(mat2):
             return None
         return [mat1[i][:] + mat2[i][:] for i in range(len(mat1))]
