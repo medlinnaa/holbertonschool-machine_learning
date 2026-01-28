c2d0681f4127d0355ac81a2ec9fa2fc9edafed91
medlinnaa <madinamamishova@gmail.com>
2026-01-28 18:08:54 +0000

    seventh task done

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""declaring a function below to concatenate """
+def cat_arrays(arr1, arr2):
+    """a function that returns
+    the concatenation of two arrays """
+    concatArr = arr1 + arr2
+    return concatArr
