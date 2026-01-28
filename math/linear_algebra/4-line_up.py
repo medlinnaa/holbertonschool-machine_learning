d35f48241246761cfee76424ff40bbd63ada14f4
medlinnaa <madinamamishova@gmail.com>
2026-01-28 11:40:23 +0000

    fifth task done

--- math/linear_algebra/4-line_up.py
+++ math/linear_algebra/4-line_up.py
@@ -3,7 +3,8 @@
 
 
 def add_arrays(arr1, arr2):
-    """a function that returns a new list with element-wise sums, or None if shapes differ."""
+    """a function that returns 
+    a new list with element-wise sums, or None if shapes differ."""
     if len(arr1) != len(arr2):
         return None
     return [arr1[i] + arr2[i] for i in range(len(arr1))]
