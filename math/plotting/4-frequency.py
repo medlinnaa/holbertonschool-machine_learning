115bc7a48f97e2aad14c3acc2d949c399423b962
medlinnaa <madinamamishova@gmail.com>
2026-01-29 13:43:31 +0400

    frequency task done

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -1,21 +1,18 @@
 #!/usr/bin/env python3
-"""4-frequency.py: Plot histogram of student grades for Project A."""
+"""4-frequency.py: Plot a histogram of student grades for Project A."""
 
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 def frequency():
-    """Display frequency histogram of student grades."""
+    """Plot a histogram of student grades with 10-point bins."""
     np.random.seed(5)
     student_grades = np.random.normal(68, 15, 50)
 
     plt.figure(figsize=(6.4, 4.8))
-    bins = np.arange(40, 101, 10)  # bins every 10 units (40–100)
-
-    plt.hist(student_grades, bins=bins, edgecolor='black')
+    plt.hist(student_grades, bins=10, range=(0, 100), edgecolor='black')
     plt.xlabel("Grades")
     plt.ylabel("Number of Students")
     plt.title("Project A")
-    plt.xlim(0, 100)  # axis range must match image
     plt.show()
