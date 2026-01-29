a111f4ae57a08beff154e240581494a158d95581
medlinnaa <madinamamishova@gmail.com>
2026-01-29 13:23:00 +0400

    line graph task done

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -2,11 +2,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def line():
     y = np.arange(0, 11) ** 3
     x = np.arange(0, 11)
-
-
     plt.figure(figsize=(6.4, 4.8))
     plt.plot(x, y, 'r-')
     plt.xlim(0, 10)
