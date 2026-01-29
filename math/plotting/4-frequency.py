c85cc78c27850eefc404736dc995be0669c097f5
medlinnaa <madinamamishova@gmail.com>
2026-01-29 13:35:38 +0400

    frequency task done

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -11,11 +11,11 @@ def frequency():
     student_grades = np.random.normal(68, 15, 50)
 
     plt.figure(figsize=(6.4, 4.8))
-    bins = np.arange(0, 101, 10)  # bins every 10 units
+    bins = np.arange(40, 101, 10)  # bins every 10 from 40 to 100
 
     plt.hist(student_grades, bins=bins, edgecolor='black')
     plt.xlabel("Grades")
     plt.ylabel("Number of Students")
     plt.title("Project A")
-    plt.xlim(0, 100)
+    plt.xlim(0, 100)  # axis still spans full range
     plt.show()
