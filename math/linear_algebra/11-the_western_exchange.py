29012db37800805ff5dbbcc9a323c804f36fe074
medlinnaa <madinamamishova@gmail.com>
2026-01-30 08:01:13 +0000

    twelfth task

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""defining a function that transposes a numpy array."""
+
+
+def np_transpose(matrix):
+    """a function that returns
+    the transpose of a numpy.ndarray."""
+    return matrix.T
