11b177f484259077911ade9f428d3f2a478fec45
medlinnaa <madinamamishova@gmail.com>
2026-01-30 08:10:18 +0000

    fifteenth task

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""performing matrix multiplication using numpy."""
+
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """a function that returns
+    the matrix product of two numpy.ndarrays."""
+    return np.matmul(mat1, mat2)
