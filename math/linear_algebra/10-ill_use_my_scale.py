ae837b195220102deef25fa5f1daaa0090d5e52e
medlinnaa <madinamamishova@gmail.com>
2026-01-30 07:59:10 +0000

    eleventh task

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""returning the shape of a numpy array."""
+
+
+def np_shape(matrix):
+    """a function that returns
+    the shape of a numpy.ndarray."""
+    return matrix.shape
