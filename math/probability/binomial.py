e2278ac4c83760112fc730e950f18825fca2bca0
medlinnaa <madinamamishova@gmail.com>
2026-02-11 11:13:16 +0000

    update the class and calculate the value of the PMF

--- math/probability/binomial.py
+++ math/probability/binomial.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""initializing Binomial distribution class"""
+"""update Binomial distribution class with PMF"""
 
 
 class Binomial:
@@ -12,10 +12,8 @@ class Binomial:
                 raise ValueError("n must be a positive value")
             if not (0 < p < 1):
                 raise ValueError("p must be greater than 0 and less than 1")
-
             self.n = int(n)
             self.p = float(p)
-
         else:
             if not isinstance(data, list):
                 raise TypeError("data must be a list")
@@ -24,9 +22,34 @@ class Binomial:
 
             mean = sum(data) / len(data)
             variance = sum((x - mean) ** 2 for x in data) / len(data)
-
             p_initial = 1 - (variance / mean)
-
             self.n = int(round(mean / p_initial))
-
             self.p = float(mean / self.n)
+
+    def pmf(self, k):
+        """finding the value of the PMF for a given number of successes"""
+        # Ensure k is an integer
+        k = int(k)
+
+        # k must be within the range [0, n]
+        if k < 0 or k > self.n:
+            return 0
+
+        # Calculate factorial for binomial coefficient: n! / (k!(n-k)!)
+        def factorial(num):
+            res = 1
+            for i in range(1, num + 1):
+                res *= i
+            return res
+
+        n_fact = factorial(self.n)
+        k_fact = factorial(k)
+        nk_fact = factorial(self.n - k)
+
+        # Binomial coefficient (n choose k)
+        combination = n_fact / (k_fact * nk_fact)
+
+        # PMF = (nCk) * (p^k) * ((1-p)^(n-k))
+        pmf_val = combination * (self.p ** k) * ((1 - self.p) ** (self.n - k))
+
+        return pmf_val
