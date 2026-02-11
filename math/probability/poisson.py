db40817655be4ae8e3c64ad9b2bc104869a8b6ae
medlinnaa <madinamamishova@gmail.com>
2026-02-11 10:10:20 +0000

    a class that represents a poisson distribution

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -1,25 +1,38 @@
 #!/usr/bin/env python3
-"""a module to represent a Poisson distribution"""
+""" updating the Poisson class to include PMF(Probability Mass Function) """
 
 
 class Poisson:
-    """here is the class that contains a poisson distribution"""
+    """class that represents a poisson distribution"""
 
     def __init__(self, data=None, lambtha=1.):
         """initializing the distribution"""
-
-        # option1: data is not provided, use the lambtha parameter
         if data is None:
             if lambtha <= 0:
                 raise ValueError("lambtha must be a positive value")
             self.lambtha = float(lambtha)
-
-        # option2: data is provided, estimate lambtha from the list
         else:
             if not isinstance(data, list):
                 raise TypeError("data must be a list")
-
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
-
             self.lambtha = float(sum(data) / len(data))
+
+    def pmf(self, k):
+        """calculating the value of the PMF for a given number of 'successes'"""
+        k = int(k)
+
+        if k < 0:
+            return 0
+
+        # e is approximately 2.7182818285
+        e = 2.7182818285
+
+        factorial = 1
+        for i in range(1, k + 1):
+            factorial *= i
+
+        # P = (e^-lambda * lambda^k) / k!
+        pdf_val = (e ** -self.lambtha) * (self.lambtha ** k) / factorial
+
+        return pdf_val
