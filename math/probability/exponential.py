c3dcd1a10a2721e5a2ef0f8bf40177a4ce64c9c3
medlinnaa <madinamamishova@gmail.com>
2026-02-11 10:28:09 +0000

    update the class Exponential and finding the value of the PDF

--- math/probability/exponential.py
+++ math/probability/exponential.py
@@ -1,24 +1,32 @@
 #!/usr/bin/env python3
-"""module to represent an exponential distribution"""
+"""updating the exponential class to include PDF"""
 
 
 class Exponential:
-    """the class that represents an exponential distribution"""
+    """class that represents an exponential distribution"""
 
     def __init__(self, data=None, lambtha=1.):
         """initializing the distribution"""
-        # Option1:no data provided
         if data is None:
             if lambtha <= 0:
                 raise ValueError("lambtha must be a positive value")
             self.lambtha = float(lambtha)
-
-        # Option2:data list provided
         else:
             if not isinstance(data, list):
                 raise TypeError("data must be a list")
-
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
-
+            # lambtha is the inverse of the mean
             self.lambtha = float(len(data) / sum(data))
+
+    def pdf(self, x):
+        """calculating the value of the PDF 
+        for a given time period"""
+        if x < 0:
+            return 0
+
+        e = 2.7182818285
+
+        pdf_val = self.lambtha * (e ** (-self.lambtha * x))
+
+        return pdf_val
