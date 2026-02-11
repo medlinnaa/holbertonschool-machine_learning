b35036a97fa030eed28e9eac0e38cad8e287acfe
medlinnaa <madinamamishova@gmail.com>
2026-02-11 16:32:19 +0000

    second bayesian task

---
+++ math/bayesian_prob/1-intersection.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+"""defining the intersection of data with various hypothetical probabilities"""
+import numpy as np
+
+
+def intersection(x, n, P, Pr):
+    """finding the intersection of obtaining x and n with each probability in P"""
+    if not isinstance(n, int) or n <= 0:
+        raise ValueError("n must be a positive integer")
+
+    if not isinstance(x, int) or x < 0:
+        raise ValueError(
+            "x must be an integer that is greater than or equal to 0"
+        )
+
+    if x > n:
+        raise ValueError("x cannot be greater than n")
+
+    if not isinstance(P, np.ndarray) or P.ndim != 1:
+        raise TypeError("P must be a 1D numpy.ndarray")
+
+    if not isinstance(Pr, np.ndarray) or Pr.shape != P.shape:
+        raise TypeError("Pr must be a numpy.ndarray with the same shape as P")
+
+    if np.any((P < 0) | (P > 1)):
+        raise ValueError("All values in P must be in the range [0, 1]")
+
+    if np.any((Pr < 0) | (Pr > 1)):
+        raise ValueError("All values in Pr must be in the range [0, 1]")
+
+    if not np.isclose(np.sum(Pr), 1):
+        raise ValueError("Pr must sum to 1")
+
+    fact_n = np.math.factorial(n)
+    fact_x = np.math.factorial(x)
+    fact_nx = np.math.factorial(n - x)
+    combination = fact_n / (fact_x * fact_nx)
+    
+    likelihood = combination * (P ** x) * ((1 - P) ** (n - x))
+
+    return likelihood * Pr
