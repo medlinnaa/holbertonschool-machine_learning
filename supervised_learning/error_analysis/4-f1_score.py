61df98c0cbe53b3afd2d93b98263b41d6cfd8be9
medlinnaa <madinamamishova@gmail.com>
2026-02-18 12:06:30 +0000

    function that calculates the F1 score of a confusion matrix

---
+++ supervised_learning/error_analysis/4-f1_score.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""Module to calculate f1-score"""
+import numpy as np
+sensitivity = __import__('1-sensitivity').sensitivity
+precision = __import__('2-precision').precision
+
+
+def f1_score(confusion):
+    """
+    Calculates the F1 score for each class
+    in a confusion matrix
+    
+    Args:
+        confusion: numpy.ndarray of shape (classes, classes)
+            row indices = correct labels
+            column indices = predicted labels
+            
+    Returns:
+        A numpy.ndarray of shape (classes,)
+        containing F1 score of each class
+    """
+    prec = precision(confusion)
+    sens = sensitivity(confusion)
+
+    f1 = 2*(prec*sens) / (prec + sens)
+
+    return f1
