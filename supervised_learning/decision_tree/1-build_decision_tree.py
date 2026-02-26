b1bd0e81101c60fe302cd3ef5786d25e9d99c36b
medlinnaa <madinamamishova@gmail.com>
2026-02-26 08:32:34 +0000

    number of nodes/leaves in a decision tree

--- supervised_learning/decision_tree/1-build_decision_tree.py
+++ supervised_learning/decision_tree/1-build_decision_tree.py
@@ -1,9 +1,22 @@
 #!/usr/bin/env python3
+"""
+a module to count the number of nodes
+in a decision tree
+"""
 import numpy as np
-"""a module to check the number of nodes
-in a decision tree"""
+
+
 class Node:
-    def __init__(self, feature=None, threshold=None, left_child=None, right_child=None, is_root=False, depth=0):
+    """represents an internal node in a decision tree"""
+    def __init__(
+        self,
+        feature=None,
+        threshold=None,
+        left_child=None,
+        right_child=None,
+        is_root=False,
+        depth=0
+    ):
         self.feature = feature
         self.threshold = threshold
         self.left_child = left_child
@@ -14,18 +27,27 @@ class Node:
         self.depth = depth
 
     def max_depth_below(self):
-        return max(self.left_child.max_depth_below(), self.right_child.max_depth_below())
+        """recursively finds the maximum depth of the tree"""
+        return max(
+            self.left_child.max_depth_below(),
+            self.right_child.max_depth_below()
+        )
 
     def count_nodes_below(self, only_leaves=False):
-        """counts nodes or leaves below this node"""
+        """recursively counts the nodes or leaves below this node"""
         left_count = self.left_child.count_nodes_below(only_leaves=only_leaves)
-        right_count = self.right_child.count_nodes_below(only_leaves=only_leaves)
+        right_count = self.right_child.count_nodes_below(
+            only_leaves=only_leaves
+        )
 
         if only_leaves:
             return left_count + right_count
+
         return 1 + left_count + right_count
 
+
 class Leaf(Node):
+    """represents a leaf node in a decision tree"""
     def __init__(self, value, depth=None):
         super().__init__()
         self.value = value
@@ -33,14 +55,24 @@ class Leaf(Node):
         self.depth = depth
 
     def max_depth_below(self):
+        """returns the depth of the leaf"""
         return self.depth
 
     def count_nodes_below(self, only_leaves=False):
-        """a leaf always counts as 1"""
+        """a leaf always counts as 1 node/leaf"""
         return 1
 
+
 class Decision_Tree():
-    def __init__(self, max_depth=10, min_pop=1, seed=0, split_criterion="random", root=None):
+    """represents the decision tree itself"""
+    def __init__(
+        self,
+        max_depth=10,
+        min_pop=1,
+        seed=0,
+        split_criterion="random",
+        root=None
+    ):
         self.rng = np.random.default_rng(seed)
         if root:
             self.root = root
@@ -54,7 +86,9 @@ class Decision_Tree():
         self.predict = None
 
     def depth(self):
+        """returns the maximum depth of the entire tree"""
         return self.root.max_depth_below()
 
     def count_nodes(self, only_leaves=False):
+        """returns the total number of nodes or leaves in the tree"""
         return self.root.count_nodes_below(only_leaves=only_leaves)
