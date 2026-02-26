b2778cc5994dbc70e62654b9a54787667d4f7beb
medlinnaa <madinamamishova@gmail.com>
2026-02-26 10:39:52 +0000

    training decision trees

--- supervised_learning/decision_tree/7-build_decision_tree.py
+++ supervised_learning/decision_tree/7-build_decision_tree.py
@@ -133,6 +133,9 @@ class Leaf(Node):
         self.depth = depth
 
     def max_depth_below(self):
+        """
+        Returns the depth of the leaf itself
+        """
         return self.depth
 
     def count_nodes_below(self, only_leaves=False):
