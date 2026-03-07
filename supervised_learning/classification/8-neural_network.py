19b270c1e8f4b2f5a5b297634eba71e77e01c479
medlinnaa <madinamamishova@gmail.com>
2026-03-07 09:26:25 +0000

    a class that defines a neural network with one hidden layer

--- supervised_learning/classification/8-neural_network.py
+++ supervised_learning/classification/8-neural_network.py
@@ -4,7 +4,9 @@ import numpy as np
 
 
 class NeuralNetwork:
-    """Class that defines a neural network for binary classification"""
+    """
+    Class that defines a neural network performing binary classification
+    """
 
     def __init__(self, nx, nodes):
         """
@@ -12,26 +14,26 @@ class NeuralNetwork:
         nx: number of input features
         nodes: number of nodes in the hidden layer
         """
-        # 1. Validation for nx
+        # 1. Validation of nx
         if not isinstance(nx, int):
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
 
-        # 2. Validation for nodes
+        # 2. Validation of nodes
         if not isinstance(nodes, int):
             raise TypeError("nodes must be an integer")
         if nodes < 1:
             raise ValueError("nodes must be a positive integer")
 
         # 3. Hidden Layer Attributes
-        # Weights: (nodes, nx), Bias: (nodes, 1)
+        # W1: (nodes, nx), b1: (nodes, 1)
         self.W1 = np.random.randn(nodes, nx)
         self.b1 = np.zeros((nodes, 1))
         self.A1 = 0
 
         # 4. Output Layer Attributes
-        # Weights: (1, nodes), Bias: 0
+        # W2: (1, nodes), b2: 0
         self.W2 = np.random.randn(1, nodes)
         self.b2 = 0
         self.A2 = 0
