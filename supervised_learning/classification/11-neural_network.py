798a929aa54db2d54607277262cd989eb24ccfbd
medlinnaa <madinamamishova@gmail.com>
2026-03-07 09:44:31 +0000

    a class that calculates the cost of the model using logistic regression

---
+++ supervised_learning/classification/11-neural_network.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python3
+"""Defines a neural network with one hidden layer"""
+import numpy as np
+
+
+class NeuralNetwork:
+    """
+    Class that defines a neural network performing binary classification
+    """
+
+    def __init__(self, nx, nodes):
+        """
+        Initializes the neural network
+        nx: number of input features
+        nodes: number of nodes in the hidden layer
+        """
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+
+        if not isinstance(nodes, int):
+            raise TypeError("nodes must be an integer")
+        if nodes < 1:
+            raise ValueError("nodes must be a positive integer")
+
+        # Private attributes of hidden layer
+        self.__W1 = np.random.randn(nodes, nx)
+        self.__b1 = np.zeros((nodes, 1))
+        self.__A1 = 0
+
+        # Private attributes of output layer
+        self.__W2 = np.random.randn(1, nodes)
+        self.__b2 = 0
+        self.__A2 = 0
+
+    @property
+    def W1(self):
+        """Getter W1"""
+        return self.__W1
+
+    @property
+    def b1(self):
+        """Getter b1"""
+        return self.__b1
+
+    @property
+    def A1(self):
+        """Getter A1"""
+        return self.__A1
+
+    @property
+    def W2(self):
+        """Getter W2"""
+        return self.__W2
+
+    @property
+    def b2(self):
+        """Getter b2"""
+        return self.__b2
+
+    @property
+    def A2(self):
+        """Getter A2"""
+        return self.__A2
+
+    def forward_prop(self, X):
+        """
+        Calculates forward propagation of the neural network
+        X: numpy.ndarray with shape (nx, m) containing input data
+        Returns: private attributes __A1 and __A2, respectively
+        """
+        # 1. Hidden Layer calculation
+        # Z1 = W1 * X + b1
+        z1 = np.matmul(self.__W1, X) + self.__b1
+        # A1 = Sigmoid activation of z1
+        self.__A1 = 1 / (1 + np.exp(-z1))
+
+        # 2. Output Layer calculation
+        # Z2 = W2 * A1 + b2
+        z2 = np.matmul(self.__W2, self.__A1) + self.__b2
+        # A2 = Sigmoid activation of z2 (final prediction)
+        self.__A2 = 1 / (1 + np.exp(-z2))
+
+        return self.__A1, self.__A2
+
+    def cost(self, Y, A):
+        """
+        Calculates the cost of the model using logistic regression
+        Y: numpy.ndarray with shape (1, m) containing correct labels
+        A: numpy.ndarray with shape (1, m) containing activated outputs
+        Returns: the cost
+        """
+        # m is the number of examples
+        m = Y.shape[1]
+
+        # Logistic regression cost formula
+        # We use 1.0000001 - A for numerical stability
+        inner_log = Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A)
+        cost = -1 / m * np.sum(inner_log)
+
+        return cost
