c011e929935230ba2356a85070ae7d048e79f53e
medlinnaa <madinamamishova@gmail.com>
2026-03-12 15:00:45 +0000

    a function that builds a model with Keras - Functional API version

--- supervised_learning/keras/1-input.py
+++ supervised_learning/keras/1-input.py
@@ -12,30 +12,28 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
     inputs = K.Input(shape=(nx,))
 
     # 2. Define the first hidden layer
-    # We call the layer like a function and pass 'inputs' into it
     reg = K.regularizers.l2(lambtha)
 
-    # This line means: Take 'inputs',
-    # pass it through 'Dense', call the result 'x'
-    x = K.layers.Dense(layers[0],
-         activation=activations[0],
+    # Fixed indentation here:
+    x = K.layers.Dense(
+        layers[0], 
+        activation=activations[0],
         kernel_regularizer=reg
-        )(inputs)
+    )(inputs)
 
     # 3. Loop through the rest of the layers
     for i in range(1, len(layers)):
         # Add Dropout to the previous output 'x'
         x = K.layers.Dropout(1 - keep_prob)(x)
 
-        # Pass the result of Dropout
-        # into the next Dense layer
-        x = K.layers.Dense(layers[i],
+        # Fixed indentation here:
+        x = K.layers.Dense(
+            layers[i], 
             activation=activations[i],
             kernel_regularizer=reg
-            )(x)
+        )(x)
 
-    # 4. Create the model by connecting
-    # the start (inputs) to the end (x)
+    # 4. Create the model
     model = K.Model(inputs=inputs, outputs=x)
 
     return model
