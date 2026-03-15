0d35fa56c80d41a015d89c4a54e859bc58824a8c
medlinnaa <madinamamishova@gmail.com>
2026-03-15 18:34:06 +0000

    a function that creates a batch normalization layer for a neural network in tensorflow

--- supervised_learning/optimization/14-batch_norm.py
+++ supervised_learning/optimization/14-batch_norm.py
@@ -1,27 +1,35 @@
 #!/usr/bin/env python3
-"""Batch Normalization"""
+"""Module to create a batch normalization layer in TensorFlow"""
 import tensorflow as tf
 
 
 def create_batch_norm_layer(prev, n, activation):
-    """Creates a batch normalization layer for a neural network in tensorflow"""
-    
-    # 1. Setup the weight initializer
+    """
+    Creates a batch normalization layer for a neural network in tensorflow
+    Args:
+        prev: activated output of the previous layer
+        n: number of nodes in the layer to be created
+        activation: activation function to be used on the output
+    Returns: a tensor of the activated output for the layer
+    """
+    # 1. Initialize weights using VarianceScaling
     init = tf.keras.initializers.VarianceScaling(mode='fan_avg')
 
-    # 2. Dense Layer WITHOUT bias
-    # If use_bias is True (default), it throws off the random seed sequence
-    model_layer = tf.keras.layers.Dense(units=n, 
-                                        kernel_initializer=init, 
+    # 2. Create the Dense layer
+    # Keeping use_bias=True changes the random seed consumption.
+    model_layer = tf.keras.layers.Dense(units=n,
+                                        kernel_initializer=init,
                                         use_bias=False)
-    
-    # Apply the linear transformation
+
     Z = model_layer(prev)
 
     # 3. Apply Batch Normalization
-    # Gamma and Beta are trainable by default, init to 1 and 0
+    # Epsilon must be exactly 1e-7 as requested
     bn_layer = tf.keras.layers.BatchNormalization(epsilon=1e-7)
+
     Z_norm = bn_layer(Z)
 
-    # 4. Apply Activation last
+    # 4. Apply the activation function LAST
+    if activation is None:
+        return Z_norm
     return activation(Z_norm)
