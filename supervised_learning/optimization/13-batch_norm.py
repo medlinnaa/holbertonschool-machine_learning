f48fa5345d565f2ab74975d58216501dedd81cd3
medlinnaa <madinamamishova@gmail.com>
2026-03-15 18:26:08 +0000

    a function that normalizes an unactivated output of a neural network using batch normalization

--- supervised_learning/optimization/13-batch_norm.py
+++ supervised_learning/optimization/13-batch_norm.py
@@ -5,7 +5,8 @@ import numpy as np
 
 def batch_norm(Z, gamma, beta, epsilon):
     """
-    Normalizes an unactivated output of a neural network using batch normalization
+    Normalizes an unactivated output of
+    a neural network using batch normalization
     Args:
         Z: numpy.ndarray of shape (m, n) to be normalized
         gamma: numpy.ndarray of shape (1, n) containing the scales
