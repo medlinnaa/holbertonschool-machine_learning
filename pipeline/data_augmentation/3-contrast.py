72a55ab176cbbc68f0cd98803310a0f34243b882
medlinnaa <madinamamishova@gmail.com>
2026-04-11 11:50:18 +0000

    a function that randomly adjusts the contrast of an image

---
+++ pipeline/data_augmentation/3-contrast.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+"""
+import tensorflow as tf
+
+
+def change_contrast(image, lower, upper):
+    """
+    Randomly adjusts the contrast of an image
+
+    Args:
+        image: a 3D tf.Tensor containing the image to adjust
+        lower: float representing the lower bound of the random contrast factor
+        upper: float representing the upper bound of the random contrast factor
+
+    Returns:
+        The contrast-adjusted image
+    """
+    return tf.image.random_contrast(image, lower, upper)
