9570452ab4d060d8793ffec2de530e8083bdfdac
medlinnaa <madinamamishova@gmail.com>
2026-07-05 18:11:19 +0200

    a function that creates, builds and trains a gensim word2vec model

--- supervised_learning/word_embeddings/2-word2vec.py
+++ supervised_learning/word_embeddings/2-word2vec.py
@@ -29,8 +29,8 @@ def word2vec_model(sentences, vector_size=100, min_count=5, window=5,
         window=window,
         negative=negative,
         sg=0 if cbow else 1,
+        epochs=epochs,
         seed=seed,
-        workers=workers,
-        epochs=epochs
+        workers=workers
     )
     return model
