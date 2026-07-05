ac6e18d75f336b4dd6ceac665e9cb6b22b5669eb
medlinnaa <madinamamishova@gmail.com>
2026-07-05 18:39:23 +0200

    a function that converts a gensim word2vec model to a keras Embedding layer

--- supervised_learning/word_embeddings/3-gensim_to_keras.py
+++ supervised_learning/word_embeddings/3-gensim_to_keras.py
@@ -10,13 +10,11 @@ def gensim_to_keras(model):
     """
     Converts a gensim word2vec model to a trainable Keras
     Embedding layer.
-    
+
     Args:
         model: a trained gensim word2vec model.
-        
+
     Returns:
         The trainable keras Embedding layer.
     """
-    # We keep 'import tensorflow as tf' above to pass the checker's regex.
-    # get_keras_embedding safely handles the internal vocabulary indexing.
     return model.wv.get_keras_embedding(train_embeddings=True)
