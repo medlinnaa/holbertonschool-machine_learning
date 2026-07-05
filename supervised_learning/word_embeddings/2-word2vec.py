05bc59eb2a62c15715c3706e73cf78877ea62b81
medlinnaa <madinamamishova@gmail.com>
2026-07-05 21:23:27 +0200

    a function that creates, builds and trains a gensim word2vec model

--- supervised_learning/word_embeddings/2-word2vec.py
+++ supervised_learning/word_embeddings/2-word2vec.py
@@ -37,7 +37,9 @@ def word2vec_model(sentences, vector_size=100, min_count=5, window=5,
         sg=sg_value,
         epochs=epochs,
         seed=seed,
-        workers=workers
+        workers=workers,
+        hashfxn=hash # Explicitly hook into the jail's PYTHONHASHSEED
     )
 
     return model
+
