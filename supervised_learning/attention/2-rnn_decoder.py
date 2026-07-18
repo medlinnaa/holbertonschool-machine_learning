0fb33e582c103e3ccea2f067a625a1872f2e9a1e
medlinnaa <madinamamishova@gmail.com>
2026-07-18 15:37:02 +0200

    a class that predicts the next word in a translation using an attention context vector

--- supervised_learning/attention/2-rnn_decoder.py
+++ supervised_learning/attention/2-rnn_decoder.py
@@ -12,7 +12,6 @@ class RNNDecoder(tf.keras.layers.Layer):
     def __init__(self, vocab, embedding, units, batch):
         """
         Class constructor
-
         Args:
             vocab: integer representing the size of the output vocabulary
             embedding: integer representing the dimensionality of the
@@ -33,7 +32,6 @@ class RNNDecoder(tf.keras.layers.Layer):
     def call(self, x, s_prev, hidden_states):
         """
         Public instance method to perform the forward pass
-
         Args:
             x: tensor of shape (batch, 1) containing the previous word in
                the target sequence as an index of the target vocabulary
@@ -41,7 +39,6 @@ class RNNDecoder(tf.keras.layers.Layer):
                     decoder hidden state
             hidden_states: tensor of shape (batch, input_seq_len, units)
                            containing the outputs of the encoder
-
         Returns:
             y: tensor of shape (batch, vocab) containing the output word
                as a one hot vector in the target vocabulary
@@ -50,29 +47,15 @@ class RNNDecoder(tf.keras.layers.Layer):
         """
         SelfAttention = __import__('1-self_attention').SelfAttention
 
-        # Instantiate attention with the units of the decoder
         attention = SelfAttention(self.gru.units)
-
-        # context vector shape: (batch, units)
         context, _ = attention(s_prev, hidden_states)
 
-        # x shape after embedding: (batch, 1, embedding)
         x = self.embedding(x)
-
-        # context expanded shape: (batch, 1, units)
         context = tf.expand_dims(context, 1)
-
-        # concat shape: (batch, 1, units + embedding)
         x = tf.concat([context, x], axis=-1)
 
-        # output shape: (batch, 1, units)
-        # s shape: (batch, units)
         output, s = self.gru(x)
-
-        # Reshape output to remove sequence dimension -> (batch, units)
         output = tf.squeeze(output, axis=1)
-
-        # y shape: (batch, vocab)
         y = self.F(output)
 
         return y, s
